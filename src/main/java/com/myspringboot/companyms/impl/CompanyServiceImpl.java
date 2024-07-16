575115e0f550d2b8b669183835358b397d9cc796
munandkotha <munandreddy@gmail.com>
2024-07-16 01:44:05 -0400

    Company Microservice first commit

---
+++ src/main/java/com/myspringboot/companyms/impl/CompanyServiceImpl.java
@@ -0,0 +1,56 @@
+package com.myspringboot.companyms.impl;
+
+import com.myspringboot.companyms.Company;
+import com.myspringboot.companyms.CompanyService;
+import com.myspringboot.companyms.repos.CompanyRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CompanyServiceImpl implements CompanyService {
+    CompanyRepository companyRepository;
+
+    public CompanyServiceImpl(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    @Override
+    public List<Company> findAllCompanies() {
+        return companyRepository.findAll();
+    }
+
+    @Override
+    public Company findByCompanyId(long id) {
+        return companyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public boolean createNewCompany(Company newCompany) {
+        if (companyRepository.save(newCompany) != null) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateCompany(long id, Company updateComp) {
+        Company company = companyRepository.findById(id).orElse(null);
+        if (company != null) {
+            company.setName(updateComp.getName());
+            company.setDescription(updateComp.getDescription());
+            companyRepository.save(company);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteCompany(long id) {
+        if (companyRepository.existsById(id)) {
+            companyRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
